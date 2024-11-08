a7251177f777fcda8d68d585190e68c4cd7eb91a
vishal-sharma-vs <vishal@ashmar.in>
2024-11-08 16:07:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const port = 8800;
+
+app.get('/',(req,res)=>{
+  res.send(`this is working. and server is running at port ${port}`)
+})
 
 app.use(cors());
 
 app.use("/youtube", require("./routes/youtube"));
 
-const port = 8800;
+
 
 app.listen(port,() => {
   console.log(`server is connected at port : ${port}`);
